506bfe354c94a3c165d068aebafdc808950f05e6
mpietrza <mpietrza@student.42barcelona.com>
2025-05-28 15:51:58 +0200

    added colors to ex02

--- ex02/objs/AForm.d
+++
@@ -1,5 +0,0 @@
-objs/AForm.o: AForm.cpp AForm.hpp Bureaucrat.hpp
-
-AForm.hpp:
-
-Bureaucrat.hpp:
--- ex02/objs/Bureaucrat.d
+++
@@ -1,5 +0,0 @@
-objs/Bureaucrat.o: Bureaucrat.cpp Bureaucrat.hpp AForm.hpp
-
-Bureaucrat.hpp:
-
-AForm.hpp:
